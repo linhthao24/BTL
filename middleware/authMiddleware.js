e2f8b06567bbff2749fe0a14d4f8bdae3d0a26d2
linhthao24 <113225411+linhthao24@users.noreply.github.com>
2023-09-18 23:13:25 +0700

    Add files via upload

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv')
+const Order = require('../src/models/OrderProduct')
+const User = require('../src/models/UserModel')
 dotenv.config()
 
 const authMiddleWare = (req, res, next) => {
@@ -34,6 +36,8 @@ const authUserMiddleWare = (req, res, next) => {
             })
         }
         const { payload } = user
+        console.log(payload)
+        console.log(userId)
         if (payload?.isAdmin || payload?.id === userId) {
             next()
         } else {
@@ -44,8 +48,52 @@ const authUserMiddleWare = (req, res, next) => {
         }
     });
 }
-
+const MiddleCancel = (req, res, next) => {
+    const token = req.headers.token.split(' ')[1]
+    const orderId = req.params.id;
+    if (!token) {
+        return res.status(401).json({
+            message: 'Token is missing',
+            status: 'ERROR'
+        });
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN, async function (err, user) {
+        if (err) {
+            return res.status(401).json({
+                message: 'Token is invalid',
+                status: 'ERROR'
+            });
+        }
+        const { payload } = user;
+        try {
+            // get information of order 
+            const order = await Order.findById(orderId);
+            //console.log(order)
+            if (!order) {
+                return res.status(404).json({
+                    message: 'Order not found',
+                    status: 'ERROR'
+                });
+            }
+            //console.log(order.user.toString())
+            if (payload.isAdmin || payload.id === order.user.toString()) {
+                next();
+            } else {
+                return res.status(403).json({
+                    message: 'Access denied',
+                    status: 'ERROR'
+                });
+            }
+        } catch (error) {
+            return res.status(500).json({
+                message: 'Internal server error',
+                status: 'ERROR'
+            });
+        }
+    });
+}
 module.exports = {
     authMiddleWare,
     authUserMiddleWare,
+    MiddleCancel,
 }
