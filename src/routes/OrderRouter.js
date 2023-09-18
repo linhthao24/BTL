af32864240f4bb08487920bde0625cad5d62727c
linhthao24 <113225411+linhthao24@users.noreply.github.com>
2023-09-18 23:16:14 +0700

    Add files via upload

--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const routers = express.Router()
 const OrderController = require('../controllers/OrderController');
-const { authUserMiddleWare, authMiddleWare } = require("../../middleware/authMiddleware");
+const { authUserMiddleWare, authMiddleWare, MiddleCancel } = require("../../middleware/authMiddleware");
 
+// tao don hang
 routers.post('/create/:id', authUserMiddleWare, OrderController.CreateOrder)
-routers.get('/get-all-order/:id', authUserMiddleWare, OrderController.GetAllOrder)
-routers.get('/get-details-order/:id', OrderController.GetDetailsOrder)
-routers.delete('/cancel-order/:id', authUserMiddleWare, OrderController.CancelOrder)
+// thong tin don hang cua 1 user
+routers.get('/getAllOrder/:id', authUserMiddleWare, OrderController.GetAllOrder)
+// details of 1 order
+routers.get('/getDetailsOrder/:id', OrderController.GetDetailsOrder)
+// huy don hang
+routers.delete('/cancelOrder/:id', MiddleCancel, OrderController.CancelOrder)
+// admin get information of all order
 routers.get('/SumOrder', authMiddleWare, OrderController.SumOrder)
 
 module.exports = routers
--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -2,19 +2,17 @@ const express = require("express");
 const routers = express.Router()
 const ProductController = require('../controllers/ProductController');
 const { authMiddleWare } = require("../../middleware/authMiddleware");
+const uploader = require('../cloudinaryConfig')
 
 
-// tạo thêm sản phẩm: các trường dữ liệu cần điền được mô tả trong productmodel
+
+routers.put('/upImage/:id', uploader.array('images', 10), authMiddleWare, ProductController.UploadImage)
+
 routers.post('/create', ProductController.CreateProduct)
-// cập nhật thông tin sản phẩm chỉ admin
 routers.put('/update/:id', authMiddleWare, ProductController.UpdateProduct)
-// Lấy thông tin chi tiết từng sản phẩm
 routers.get('/getDetails/:id', ProductController.GetDetailsProduct)
-// xóa sản phẩm, chỉ có amin
 routers.delete('/delete/:id', authMiddleWare, ProductController.DeleteProduct)
-// lấy thông tin tất cả sản phẩm
 routers.get('/getAll', ProductController.GetAllProduct)
-// lấy thông tin loại của từng sản phẩm
 routers.get('/getAllType', ProductController.GetAllType)
 
 module.exports = routers
